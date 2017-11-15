1d2413fa2aec6f259da3e777d246a2e3b8e705bf
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-15 12:53:45 -0600

    discovered .val()

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -2,7 +2,8 @@
 $(document).ready(function(){
 
   $('.new-tweet textarea').on('input', function(){
-    let text = $(this)[0].value;
+    let text = $(this).val();
+    // let text = $(this)[0].value;
     $('.counter').text(140 - text.length)
                  .toggleClass('tooLong', text.length > 140);
   });
