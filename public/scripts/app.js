13893b611cf2881c465a04e761ffd3d7da33721e
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-14 18:56:36 -0600

    escaped user-generated text

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -52,22 +52,30 @@ var data = [
 
 
 
-
+function escape(str) {
+  //given to us by lhl.  Used to prevent people from saying their name is
+    // `<script> alert('hi') </script`
+    //and messing up our code.
+  //note that using jQuery .text() is a safe way to do this as well.
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 function createTweetElement(tweetInfo){
 
   return $(`
     <article class="tweet-container">
       <header class="tweet-header">
-        <img class="profile-pic" src=${tweetInfo.user.avatars.regular}>
-        <h2 class="profile-name">${tweetInfo.user.name}</h2>
-        <span class="profile-handle">${tweetInfo.user.handle}</span>
+        <img class="profile-pic" src=${escape(tweetInfo.user.avatars.regular)}>
+        <h2 class="profile-name">${escape(tweetInfo.user.name)}</h2>
+        <span class="profile-handle">${escape(tweetInfo.user.handle)}</span>
       </header>
       <section>
-        <p>${tweetInfo.content.text}</p>
+        <p>${escape(tweetInfo.content.text)}</p>
       </section>
       <footer class="tweet-footer">
-        Created: ${tweetInfo.created_at}
+        Created: ${escape(tweetInfo.created_at)}
         <span class="tweet-actions">
              🚩 🔃 💖
         </span>
